1c89ab6ce1a608be05db63ed581f0e681d33400e
BasGef <sebastiaanvangeffen@gmail.com>
2022-12-04 12:15:40 +0100

    Question 2b solved

--- 2022/day02/day02.py
+++ 2022/day02/day02.py
@@ -28,7 +28,7 @@ class Hand(IntEnum):
     Z = SCISSORS
 
 
-def win_lose(other_hand, own_hand):
+def win_lose(other_hand: Hand, own_hand: Hand) -> Result:
     # There are only 3 choices, and in each case choice loses to choice + 1
     # (e.g. Rock < Paper; Paper < Scissors; Scissors < Rock).
     # So we can use mod 3 to determine results:
@@ -38,13 +38,41 @@ def win_lose(other_hand, own_hand):
         return Result.LOSS
     return Result.WIN
 
+def load_data(file_path: str):
+    with open(file_path) as file:
+        data = file.read().splitlines()
+    return data
+
+
+def q2a():
+    matches = load_data(FILE_PATH)
+    matches = [tuple(Hand[h] for h in match.split()) for match in matches]
+    return sum(win_lose(*match) for match in matches) * 3 + \
+           sum(match[1] for match in matches) + len(matches)  # Hands are base 0 -> add 1 per match
+
+
+def elf_strategy(opponent_hand, strategy):
+    """
+    Returns the score of a match when using the elf's strategy as given in
+    question 2b.
+    """
+    if strategy == 'Y':  # Tie
+        return Hand[opponent_hand] + 1 + Result.TIE * 3
+    if strategy == 'X':  # Lose
+        return (Hand[opponent_hand] - 1) % 3 + 1
+    return (Hand[opponent_hand] + 1) % 3 + 1 + Result.WIN * 3
+
+
+def q2b():
+    matches = load_data(FILE_PATH)
+    return sum(elf_strategy(*match.split()) for match in matches)
+
 
 if __name__ == '__main__':
-    with open(FILE_PATH, 'r') as file:
-        matches = file.read().splitlines()
+    print(f'{q2a()=}')
+    print(f'{q2b()=}')
 
-    score = len(matches)  # Hands base 0; scoring begins at 1 -> add 1 per match
-    for match in matches:
-        hands = [Hand[h] for h in match.split()]
-        score += win_lose(*hands) * 3
-        score += hands[1]
+
+
+
+
