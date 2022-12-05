a9c1f35095dbfa1ed83d57be9d7f6c1fd1db7730
BasGef <sebastiaanvangeffen@gmail.com>
2022-12-05 23:05:47 +0100

    Question 5a solved

--- 2022/day05/day05.py
+++ 2022/day05/day05.py
@@ -3,29 +3,41 @@ https://adventofcode.com/2022/day/5
 """
 
 import re
+from typing import Tuple, List
 
 FILE_PATH = r'./input.txt'
 
-def load_data(file_path: str):
+
+def load_data(file_path: str) -> Tuple[List[str], List[str]]:
     with open(file_path) as file:
         data = file.read().splitlines()
     delim = data.index('')  # empty line delineates the two data sets
     return data[:delim], data[delim+1:]
 
 
-def clean_stacks_data(stacks):
+def clean_stacks_data(stacks: List[str]) -> List[List[str]]:
     stacks = reversed(stacks[:-1])
     stacks = [row[1::4] for row in stacks]
     stacks = [list(col) for col in zip(*stacks)]  # 'pivots' the lists.
     return [s if ' ' not in s else s[:s.index(' ')] for s in stacks]  # remove empties
 
 
-def clean_moves_data(moves):
+def clean_moves_data(moves: List[str]) -> List[Tuple[int, int, int]]:
     return [tuple(int(d) for d in re.findall(r'\d+', move)) for move in moves]
 
 
-stacks, moves = load_data(FILE_PATH)
-stacks = clean_stacks_data(stacks)
-moves = clean_moves_data(moves)
+def q5a():
+    stacks, moves = load_data(FILE_PATH)
+    stacks = clean_stacks_data(stacks)
+    moves = clean_moves_data(moves)
+
+    # TODO: There must be a more efficient way than this?
+    for (num, fro, to) in moves:
+        stacks[to-1] += stacks[fro-1][:-num-1:-1]
+        stacks[fro-1] = stacks[fro-1][:-num]
+
+    return ''.join(stack[-1] for stack in stacks)
 
 
+if __name__ == '__main__':
+    print(f'{q5a()=}')
