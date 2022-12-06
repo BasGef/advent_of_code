7d818aff5f128cb4badb2d4a5c7be15fa71ebdb4
BasGef <sebastiaanvangeffen@gmail.com>
2022-12-06 18:59:37 +0100

    Question 6b solved

--- 2022/day06/day06.py
+++ 2022/day06/day06.py
@@ -7,11 +7,11 @@ from collections import deque
 FILE_PATH = r'./input.txt'
 
 
-def find_marker(file_path: str) -> int:
+def find_marker(file_path: str, marker_len=4) -> int:
     with open(file_path) as file:
         buffer = deque(file.read(1))
         pos = 1
-        while len(buffer) < 4:
+        while len(buffer) < marker_len:
             pos += 1
             next_char = file.read(1)
             while next_char in buffer:
@@ -21,8 +21,13 @@ def find_marker(file_path: str) -> int:
 
 
 def q6a():
-    return find_marker(FILE_PATH)
+    return find_marker(FILE_PATH, 4)
+
+
+def q6b():
+    return find_marker(FILE_PATH, 14)
 
 
 if __name__ == '__main__':
     print(f'{q6a()=}')
+    print(f'{q6b()=}')
