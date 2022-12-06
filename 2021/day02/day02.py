ed49a9a8c4c34718e787235ff42ab388eff9be89
BasGef <sebastiaanvangeffen@gmail.com>
2022-12-06 20:34:55 +0100

    Question 2021.2b

--- 2021/day02/day02.py
+++ 2021/day02/day02.py
@@ -18,11 +18,30 @@ def get_position(moves):
     return horizontal, depth
 
 
+def get_position_aim(moves):
+    horizontal, aim, depth = 0, 0, 0
+    for move in moves:
+        direction, step = move.split(' ')
+        if direction == 'forward':
+            horizontal += int(step)
+            depth += aim * int(step)
+            continue
+        aim += int(step) * (1 if direction == 'down' else -1)
+    return horizontal, depth
+
+
 def q2a():
     moves = load_data(FILE_PATH)
     horiz, depth = get_position(moves)
     return horiz * depth
 
 
+def q2b():
+    moves = load_data(FILE_PATH)
+    horiz, depth = get_position_aim(moves)
+    return horiz * depth
+
+
 if __name__ == '__main__':
     print(f'{q2a()=}')
+    print(f'{q2b()=}')
