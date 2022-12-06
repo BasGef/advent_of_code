0a98c654090cc164dc5d89c82645217c98060b6d
BasGef <sebastiaanvangeffen@gmail.com>
2022-12-06 22:02:08 +0100

    Question 2021.3a

--- 2021/day03/day03.py
+++ 2021/day03/day03.py
@@ -3,15 +3,30 @@ from statistics import mode
 FILE_PATH = './input.txt'
 
 
-def q3a():
-    with open(FILE_PATH) as f:
+def load_data_as_bits(file_path):
+    with open(file_path) as f:
         data = [[int(c) for c in l] for l in f.read().splitlines()]
+    return data
+
+
+def most_common_bit_value(data):
     bits = [list(d) for d in zip(*data)]  # pivot number to bit positions
-    most_sig = [mode(bit) for bit in bits]
-    gamma = sum(2 ** pow for pow, is_set in enumerate(reversed(most_sig)) if is_set)
-    epsilon = sum(2 ** pow for pow, is_set in enumerate(reversed(most_sig)) if not is_set)
-    return gamma * epsilon
+    return [mode(bit) for bit in bits]
+
+
+def gamma(bits):
+    return sum(2 ** pow for pow, is_set in enumerate(reversed(bits)) if is_set)
+
+
+def epsilon(bits):
+    return sum(2 ** pow for pow, is_set in enumerate(reversed(bits)) if not is_set)
+
+
+def q3a():
+    data = load_data_as_bits(FILE_PATH)
+    bits = most_common_bit_value(data)
+    return gamma(bits) * epsilon(bits)
 
 
 if __name__ == '__main__':
-    print(f'{q3a=}')
+    print(f'{q3a()=}')
