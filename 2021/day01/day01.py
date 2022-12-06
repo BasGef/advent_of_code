6274105bd22354f0a6c56ef838ed07222e03147d
BasGef <sebastiaanvangeffen@gmail.com>
2022-12-06 19:45:45 +0100

    Question 2021.1b

--- 2021/day01/day01.py
+++ 2021/day01/day01.py
@@ -2,12 +2,23 @@ FILE_PATH = './input.txt'
 
 from itertools import pairwise
 
+def load_data(file_path):
+    with open(file_path) as file:
+        data = [int(x) for x in file.read().splitlines()]
+    return data
+
 
 def q1a():
-    with open(FILE_PATH) as file:
-        data = [int(x) for x in file.read().splitlines()]
+    data = load_data(FILE_PATH)
     return sum(b > a for a, b in pairwise(data))
+
+
+def q1b():
+    data = load_data(FILE_PATH)
+    # Compare overlapping windows->we only have to compare the outermost items
+    return sum(b > a for a, b in zip(data, data[3:]))
 
 
 if __name__ == '__main__':
     print(f'{q1a()=}')
+    print(f'{q1b()=}')
