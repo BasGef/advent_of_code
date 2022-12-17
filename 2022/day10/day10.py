a17dec1e6ee93d5de59515a39909b66a86c69e86
BasGef <sebastiaanvangeffen@gmail.com>
2022-12-17 16:17:33 +0100

    Question 2022.10b

--- 2022/day10/day10.py
+++ 2022/day10/day10.py
@@ -1,11 +1,15 @@
+import numpy as np
+
 FILE_PATH = r'./input.txt'
 
 
 class CathodeRayTube:
-    def __init__(self, start_value=1):
+    def __init__(self, start_value=1, screen_size=(6, 40)):
         self._register = start_value
         self.signal_strength = []
         self._cycle = 0
+        self.screen_x = screen_size[1]
+        self._display = np.zeros(screen_size, str)
 
     def check_cycle(self):
         if self._cycle % 40 != 20:
@@ -15,6 +19,7 @@ class CathodeRayTube:
     def noop(self):
         self._cycle += 1
         self.check_cycle()
+        self.draw()
 
     def add(self, value):
         self.noop()
@@ -28,6 +33,15 @@ class CathodeRayTube:
             value = int(command.split(' ')[1])
             self.add(value)
 
+    def draw(self):
+        y, x = divmod(self._cycle - 1, self.screen_x)
+        pixel = '#' if abs(x - self._register) <= 1 else ' '
+        self._display[y][x] = pixel
+
+    @property
+    def display(self):
+        return '\n'.join([''.join(line) for line in self._display])
+
 
 def load_data(file_path):
     with open(file_path) as file:
@@ -35,12 +49,14 @@ def load_data(file_path):
     return data
 
 
-def q10a():
+def q10():
     crt = CathodeRayTube()
     for command in load_data(FILE_PATH):
         crt.do_command(command)
-    return sum(crt.signal_strength)
+    q10a = sum(crt.signal_strength)
+    print(f'{q10a=}')
+    print('q10b=\n' + crt.display)
 
 
 if __name__ == '__main__':
-    print(f'{q10a()=}')
+    q10()
