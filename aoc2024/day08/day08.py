f56a6ed1ddc69b0ac5881502d8a52f133ecb401a
BasGef <sebastiaanvangeffen@gmail.com>
2024-12-14 15:45:35 +0100

    Problem 2024.08 part 2

--- aoc2024/day08/day08.py
+++ aoc2024/day08/day08.py
@@ -1,6 +1,9 @@
 import itertools
 import numpy as np
 
+# 1317 too low
+#
+
 FILE_NAME = r'aoc2024/day08/input.txt'
 
 def read_data():
@@ -10,13 +13,38 @@ def read_data():
     return data
 
 
-def find_antinodes(antenna_type):
+def find_antinodes(antenna_type, resonance=False, map_size=None):
     antinodes = set()
     antennas = [np.array((x, y)) for x, y in zip(*np.where(data==antenna_type))]
     for a0, a1 in itertools.combinations(antennas, 2):
         distance = a1 - a0
-        antinodes.add(tuple(a0 - distance))
-        antinodes.add(tuple(a1 + distance))
+        if not resonance:
+            antinodes.add(tuple(a0 - distance))
+            antinodes.add(tuple(a1 + distance))
+            continue
+        origin = (np.array(map_size) - 1) * (distance < 0)
+        start = origin + ((a0 - origin) / distance - int(min((a0 - origin) / distance))) * distance
+        nodes = {tuple(np.round(start + i * distance, 0)) for i in range(max(map_size))}
+        antinodes = antinodes.union(nodes)
+    return antinodes
+
+
+# def antinodes_resonance(antenna_type, map_size):
+#     antinodes = set()
+#     antennas = [np.array((x, y)) for x, y in zip(*np.where(data==antenna_type))]
+#     for a0, a1 in itertools.combinations(antennas,2):
+#         distance = a1 - a0
+#         origin = (np.array(map_size) - 1) * (distance < 0)
+#         start = origin + ((a0 - origin) / distance - int(min((a0 - origin) / distance))) * distance
+#         antinodes = antinodes.union({tuple(np.round(start + i * distance, 0)) for i in range(max(map_size))})
+#     return antinodes
+
+
+def flatten_and_filter(antinodes, map_size):
+    antinodes = set(itertools.chain.from_iterable(antinodes))
+    antinodes = list(filter(
+        lambda x: (0 <= x[0] < map_size[0]) and (0 <= x[1] < map_size[1]),
+        antinodes))
     return antinodes
 
 
@@ -25,10 +53,12 @@ if __name__ == '__main__':
 
     chars = np.unique(data)
     antinodes = [find_antinodes(char) for char in chars if char != '.']
-    antinodes = set(itertools.chain.from_iterable(antinodes))
-    antinodes_on_map = list(filter(
-        lambda x: (0 <= x[0] < data.shape[0]) and (0 <= x[1] < data.shape[1]),
-        antinodes))
+    antinodes = flatten_and_filter(antinodes, data.shape)
+    answer_8a = len(antinodes)
 
-    answer_8a = len(antinodes_on_map)
+    antinodes_resonance = [find_antinodes(char, True, data.shape) for char in chars if char != '.']
+    antinodes_resonance = flatten_and_filter(antinodes_resonance, data.shape)
+    answer_8b = len(antinodes_resonance)
+
     print(f'{answer_8a=}')
+    print(f'{answer_8b=}')
