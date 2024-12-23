b855606f22e5fe4bd1b2802a9e32b6a1cf15127a
BasGef <sebastiaanvangeffen@gmail.com>
2024-12-23 17:51:55 +0100

    Problem 2024.20 part 2

--- aoc2024/day20/day20.py
+++ aoc2024/day20/day20.py
@@ -35,6 +35,7 @@ def compute_distance_to_exit(grid):
             break  # no need to check other directions
     return distance
 
+
 def find_shortcuts(grid, distance):
     shortcuts_lr = np.where(
         grid == '#', np.abs(np.roll(distance, 1) - np.roll(distance, -1)) - 2, np.nan)
@@ -45,8 +46,26 @@ def find_shortcuts(grid, distance):
     return shortcuts
 
 
+def count_cheats(distances, max_cheat_time, min_time_saved):
+    cols, rows = np.meshgrid(np.arange(grid.shape[0]), np.arange(grid.shape[1]))
+    cheats = 0
+    # No need to check any cell with distance < min_time_saved
+    for track_position in range(min_time_saved + 1,
+                                int(max(distances[~np.isnan(distances)])) + 1):
+        row, col = np.where(distances == track_position)
+        cheat_list = np.where(
+            (abs(rows - row) + abs(cols - col) <= max_cheat_time) &
+            (distances + abs(rows - row) + abs(cols - col) <= track_position - min_time_saved))
+        cheats += len(cheat_list[0])
+    return cheats
+
+
 if __name__ == '__main__':
     grid = read_data()
     distance = compute_distance_to_exit(grid)
     shortcuts = find_shortcuts(grid, distance)
     answer_20a = (shortcuts >= 100).sum()
+    answer_20b = count_cheats(distance, 20, 100)
+
+    print(f'{answer_20a}')
+    print(f'{answer_20b}')
